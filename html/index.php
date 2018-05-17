0f1d9bd043840a5d2cac632d8f213ae2210fec1c
root <root@renzix.pro>
2018-05-17 18:28:59 -0400

    First commit for website

---
+++ html/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+    <?php include "sidebar.html" ?>
+    <div id=main>
+    <?php include "topbar.html" ?>
+        <head>
+            <?php include "header.html" ?>
+        </head>
+        <body> 
+            <div id="ren-content">
+                <h1>Welcome!!!</h1>
+                <p>Hi this is my person website I made in order to find out how to run a server,
+                learn html/css/js and motivate myself to make projects that I finish. I want to 
+                continue working on this server adding more features seen below.</p>
+                <div class="ren-post">
+                    <div class="ren-post-head">Curently Working On...</div>
+                    <ul class="ren-post-ul">
+                        <li>University</li>
+                        <li>Learning Rust</li>
+                    </ul>
+                </div>
+                <div class="ren-post">
+                    <div class="ren-post-head">CS Bucket List</div>
+                    <ul class="ren-post-ul">
+                        <li>Make my own linux distro(probably using portage or pacman)</li>
+                        <li>Be able to manage a server easily using multiple technologies</li>
+                        <li>Make a kernel that can at least compile gcc (possibly out of rust???)</li>
+                        <li>Make a really nice website beutiful html/css/js/django, rust http daemon and prob run gentoo</li>
+                        <li>Learn C ,C++, Rust and Ada at a high level</li>
+                        <li>Learn Java, Python, Golang, html/css, and nodejs at a high level</li>
+                        <li>Become vim god</li>
+                        <li>Actually contribute or make a opensource project used by people</li>
+                    </ul>
+                </div>
+                <div class="ren-post">
+                    <div class="ren-post-head">Planned Website Features</div>
+                    <ul class="ren-post-ul">
+                        <li>Accounts</li>
+                        <li>Email(with website client)</li>
+                        <li>Make website acutally look good</li>
+                    </ul>
+                </div>
+            </div>
+        </body>
+        <?php include "footer.html"?>
+    </div>
+</html>
---
+++ html/oldwebsite/projects.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html>
+  <?php include 'header.php' ?>
+  <link rel="stylesheet" type="text/css" href="projects.css">
+  <body>
+    <h2 class="text-center">Projects</h2>
+    <div class="col-md-1"></div>
+    <div class="panel-group col-md-10">
+      
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <a href="linux.php"><h4 class="panel-title">Linux</h4></a>
+        </div>
+      </div>
+      
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <a data-toggle="collapse" href="#web-collapse"><h4 class="panel-title">Websites</h4></a>
+        </div>
+        <div id="web-collapse" class="panel-collapse collapse">
+          <a class="panel-link" href=#> <div class="panel-body">Website 1</div></a>
+          <a class="panel-link" href=#> <div class="panel-body">Website 2</div></a>
+          <a class="panel-link"href=#> <div class="panel-body">Website 3</div></a>
+        </div>
+      </div>
+    
+    </div>
+  </body>
+</html>
